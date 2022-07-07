fe9e26650af52f343f3bc493048646b5c25332c7
aturps20 <97473431+aturps20@users.noreply.github.com>
2022-07-07 21:21:40 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var a = "leela turpin";
+console.log(a);
+
+var b = "is a poo";
+console.log(b);
+
+var c = a.concat(b);
+console.log(c);
+
+var d = 34;
+console.log(d);
+var e = 35;
+console.log(e);
+
+var f = d + e
+console.log(f)
+
+
+
